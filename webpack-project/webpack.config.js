bba5b4c870e7d8f96b71dca4517056e7588dc454
Volodymyr Vyshnevetskyi <73136976+Ohsudden@users.noreply.github.com>
2024-10-08 18:49:58 +0300

    Add files via upload

---
+++ webpack-project/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // Імпорт HtmlWebpackPlugin
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',  
+  stats: {
+    children: true, 
+  },
+  entry: {
+    main: path.resolve(__dirname, './src/index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i, 
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i, 
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+          {
+              from: path.resolve(__dirname, 'src/pages/assets/images'),
+              to: path.resolve(__dirname, 'dist/assets/images')
+          }
+      ]
+  }),
+  new MiniCssExtractPlugin({
+    filename: './css/style.css', 
+  }),
+  ],
+  devServer: {
+    static: path.resolve(__dirname, './dist'), 
+    port: 8080, 
+    open: true, 
+    hot: true, 
+    compress: true, 
+  },
+};
